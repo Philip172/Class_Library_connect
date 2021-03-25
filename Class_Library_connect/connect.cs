6d9529a50303c4dc5c6c79c6b026287cb5dbe049
fill shmykov <fshmykov@gmail.com>
2021-03-25 13:34:12 +0400

    Add project files.

---
+++ Class_Library_connect/connect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Class_Library_connect
+{
+    public static class Connect
+    {
+        // порт для приема входящих запросов
+        public static int port = 8005;
+
+        // IP адрес игрока
+        public static readonly IPAddress IPAddress = IPAddress.Parse("127.0.0.1");
+    }
+
+    public static class Players_data
+    {
+        //Деньги Игроков
+        public static int[] moneys = new int[3] { 0, 0, 0, };
+
+        //Идентификаторы игроков
+        public static string[] players = new string[] { "_money1", "_money2", "_money3" };
+    }
+}
